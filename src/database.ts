2f491840f2ab82c020090e9f8926e5eca64e44df
myllenavieira <vieirasilvamyllena@gmail.com>
2023-03-15 21:02:50 -0300

    Aprofundando express com crud

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { ACCOUNT_TYPE, TAccount } from "./types";
+import { ACCOUNT_TYPE, TAccount, TGroup, GROUP_TYPE } from "./types";
 
 export const accounts: TAccount[] = [
     {
@@ -18,5 +18,29 @@ export const accounts: TAccount[] = [
         ownerName: "Fulana",
         balance: 20000,
         type: ACCOUNT_TYPE.PLATINUM
+    }
+]
+
+export const groups: TGroup[] = [
+    {
+        id: "gg001",
+        name: "Twice",
+        qtdMembers: 9,
+        debutDate: 2015,
+        type: GROUP_TYPE.GIRL
+    },
+    {
+        id: "bg001",
+        name: "BTS",
+        qtdMembers: 7,
+        debutDate: 2013,
+        type: GROUP_TYPE.BOY
+    },
+    {
+        id: "b001",
+        name: "Kard",
+        qtdMembers: 4,
+        debutDate: 2017,
+        type: GROUP_TYPE.BOTH
     }
 ]
--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,17 @@ export type TAccount = {
     balance: number,
     type: ACCOUNT_TYPE
 }
+
+export enum GROUP_TYPE {
+    GIRL = "Girlgroup",
+    BOY = "Boygroup",
+    BOTH = "Both Genders group"
+}
+
+export type TGroup = {
+    id: string,
+    name: string,
+    qtdMembers: number,
+    debutDate: number,
+    type: GROUP_TYPE
+}
